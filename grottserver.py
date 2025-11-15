dc1d6d7c5695cae82839c9ca70f4dea5b0139687
HenryJanssen <hj1704@live.nl>
2025-11-15 13:04:22 +0100

    Added debug information use deviceid (inverterno) in inverter commands to update registers

--- grottserver.py
+++ grottserver.py
@@ -236,6 +236,7 @@ class loggerInfo:
     
     def get_inverter_byinverterno(self, inverterno):
         for inverterid, inverter in self.inverters.items():
+            logger.info(f"Checking inverter {inverterid} with number {inverter.inverterno} against {inverterno}")
             if inverter.inverterno == inverterno: 
                 return inverter
         return None
